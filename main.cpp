9f09dd4be302d5ce9198a82a81b45ed757b7030c
bhbae0406 <bbae@umich.edu>
2017-01-17 14:04:00 -0500

    first step towards rapidXML

--- main.cpp
+++ main.cpp
@@ -7,9 +7,28 @@
 //
 
 #include <iostream>
+#include "rapidxml.hpp"
+#include "rapidxml_utils.hpp"
+
+using namespace rapidxml;
+
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
+    rapidxml::file<> xmlFile("0033.xml"); // Default template is char
+    rapidxml::xml_document<> doc;
+    doc.parse<0>(xmlFile.data());
+    
+   
+    std::cout
+    << "Name of my first node is: " << doc.first_node()->name() << "\n";
+    xml_node<> *node = doc.first_node("foobar");
+    std::cout << "Node foobar has value " << node->value() << "\n";
+    for (xml_attribute<> *attr = node->first_attribute();
+         attr; attr = attr->next_attribute())
+    {
+        std::cout << "Node foobar has attribute " << attr->name() << " ";
+        std::cout << "with value " << attr->value() << "\n";
+    }
     std::cout << "Hello, World!\n";
     return 0;
 }
