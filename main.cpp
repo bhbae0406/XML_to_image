50cfec8edda4cf2fbffe7e686498728d2a10f35f
bhbae0406 <bbae@umich.edu>
2017-01-17 13:31:27 -0500

    start

---
+++ main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  XML_to_jpeg
+//
+//  Created by Bae Byoung Hyun on 1/17/17.
+//  Copyright © 2017 Bae Byoung Hyun. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
